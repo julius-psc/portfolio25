a7cba444ffc1e40aa325bd0dab61e40dc3c3feb8
Julius Peschard <juliuspeschard@MacBook-Air-de-Julius.local>
2025-01-19 10:53:18 +0100

    Path names fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import {BrowserRouter, Routes, Route} from "react-router"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="*" element={<App />}/>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
