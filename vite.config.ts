2e32fa3931d148ccfc73cab345678fbfb07afa4a
Julius Peschard <juliuspeschard@MacBook-Air-de-Julius.local>
2025-01-19 10:03:28 +0100

    Custom domain name

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio25/",
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  base: '/portfolio25/',
+});
